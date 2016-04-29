d21ec543983e5d661914af1e2f36a7b137dca569
Hao Nguyen <haonv@designveloper.com>
2016-04-29 13:20:19 +0700

    fisrt commit

---
+++ server/methods/items.js
@@ -0,0 +1,27 @@
+import {Categories, Items} from '/lib/collections';
+import {Meteor} from 'meteor/meteor';
+import {check} from 'meteor/check';
+
+export default function () {
+  Meteor.methods({
+    'categories.create'(name) {
+      check(name, String);
+      const createdAt = new Date();
+      const category = {name, createdAt};
+      Categories.insert(category);
+    },
+    'items.create'(name, description, due) {
+      check(name, String);
+      check(description, String);
+      check(due, String);
+      const createdAt = new Date();
+      const item = {name, description, due,createdAt};
+      Items.insert(item);
+    },
+    'items.markComplete'(complete, itemId) {
+      check(complete, Boolean);
+      check(itemId, String);
+      Items.update(itemId, {$set: {complete: complete}})
+    }
+  });
+}
